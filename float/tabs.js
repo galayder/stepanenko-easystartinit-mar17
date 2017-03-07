e49ad205b995233864021be601c7d7ce80af1800
galayder <lego904@yandex.ru>
2017-03-07 15:06:51 +0200

    First upload

---
+++ float/tabs.js
@@ -0,0 +1,8 @@
+function openTab(layoutNumber) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    document.getElementById(layoutNumber).style.display = "block"; 
+}
